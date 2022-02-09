44e235e9cd9bf7fde7d9ae6a72de010ecf61c498
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-02-09 10:55:50 +0100

    Cmake update for lifecycle

---
+++ src/follow-wall-cavros/LifeCycle.cpp
@@ -0,0 +1,66 @@
+// Copyright 2020 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "follow-wall-cavros/LifeCycle.hpp"
+
+LyfeCicle::LifeCycle() 
+: LifecycleNode("lifecycle_node_example")
+{
+
+}
+
+using CallbackReturnT =
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+CallbackReturnT 
+LifeCycle::on_configure(const rclcpp_lifecycle::State & state)
+{
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT 
+LifeCycle::on_activate(const rclcpp_lifecycle::State & state)
+{
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT 
+LifeCycle::on_deactivate(const rclcpp_lifecycle::State & state)
+{
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT 
+LifeCycle::on_cleanup(const rclcpp_lifecycle::State & state)
+{
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT 
+LifeCycle::on_shutdown(const rclcpp_lifecycle::State & state)
+{
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT 
+LifeCycle::on_error(const rclcpp_lifecycle::State & state)
+{
+    return CallbackReturnT::SUCCESS;
+}
+
+void 
+LifeCycle::do_work()
+{
+
+}
